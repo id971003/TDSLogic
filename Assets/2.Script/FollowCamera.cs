e334b3821c0539fc4037885cf397a419c9dd8b83
kim <id971003@gmail.com>
2025-07-14 15:51:01 +0900

    add Follow Camera

---
+++ Assets/2.Script/FollowCamera.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] private Transform target;// The target to follow
+    
+    [SerializeField] private bool useFixedUpdate = false; // Use FixedUpdate for smoother physics-based movement
+    private void LateUpdate()
+    {
+        if (useFixedUpdate)
+        {
+            // Use FixedUpdate for smoother physics-based movement
+            FollowTarget();
+        }
+    }
+    void FollowTarget()
+    {
+        if (target == null) return; // If no target, do nothing
+        Vector3 desiredPosition = target.position + Data.CameraOffSet;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, Data.CameraSpeed);
+        transform.position = smoothedPosition;
+    }
+
+
+}
